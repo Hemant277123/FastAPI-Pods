6e5d20cacc6d34ff42fc48b5e906b99f8759a896
Hemant <100hemantpandey@gmail.com>
2024-07-06 12:45:19 +0530

    first commit

---
+++ src/main/java/com/example/springbootk8sdemousingyaml/SpringBootK8sDemoUsingYamlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootk8sdemousingyaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sDemoUsingYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootK8sDemoUsingYamlApplication.class, args);
+	}
+
+}
